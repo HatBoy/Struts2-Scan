11e903b10769a71e3b8ae4ad4574c02d9487bad6
HatBoy <1174232716@qq.com>
2019-09-10 09:40:41 +0800

    Update Struts2Scan.py

--- Struts2Scan.py
+++ Struts2Scan.py
@@ -1494,7 +1494,7 @@ def main(info, version, url, file, name, data, header, encode, proxy, exec, reve
     if name and url:
         # 指定漏洞利用
         name = name.upper().replace('-', '_')
-        if name not in s2_dict:
+        if name not in s2_list:
             click.secho("[ERROR] 暂不支持{name}漏洞利用".format(name=name), fg="red")
             exit(0)
         s = s2_dict[name](url, data, header, encode)
